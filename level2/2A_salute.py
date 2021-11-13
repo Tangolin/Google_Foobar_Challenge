e5b03621696b147760e5579c6e4f4105139b224e
Siteng <65639845+Tangolin@users.noreply.github.com>
2021-11-13 18:03:12 +0800

    Add files via upload

---
+++ level2/2A_salute.py
@@ -0,0 +1,8 @@
+def solution(s):
+    s = list(s)
+    positions = [index for index, minion in enumerate(s) if minion == '>']
+    salutes = sum([s[index:].count('<') for index in positions])
+    
+    return salutes * 2
+
+print(solution('-------'))
